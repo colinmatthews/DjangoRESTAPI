426065109a4d436ab9a4940d51ba59bae3b38e47
colinmatthews <matth11f@uwindsor.ca>
2016-07-12 20:56:37 -0400

    REmoved static dirs

--- Walk_inExpressAPI/settings.py
+++ Walk_inExpressAPI/settings.py
@@ -129,11 +129,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static', 'static_root')
 
-STATICFILES_DIRS = (
 
-    os.path.join(BASE_DIR, 'static', 'static_dirs'),
-
-)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static', 'media')
 MEDIA_URL = '/media/'
