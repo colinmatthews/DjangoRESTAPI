aa2231506152e15bada8a3a5c9a3c5bdb5a50667
colinmatthews <matth11f@uwindsor.ca>
2016-09-12 11:28:25 -0400

    Changes database settings to work for remote

--- Walk_inExpressAPI/settings.py
+++ Walk_inExpressAPI/settings.py
@@ -77,16 +77,16 @@ WSGI_APPLICATION = 'Walk_inExpressAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'clinicsAPI',
-        'USER': 'admin',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+  #      'NAME': 'clinicsAPI',
+  #      'USER': 'admin',
+  #      'PASSWORD': 'admin',
+  #      'HOST': 'localhost',
+  #      'PORT': '5432'
+  #  }
+#}
 
 
 # Password validation
